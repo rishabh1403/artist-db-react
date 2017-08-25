51933123e95a4415a13af67df6dac927607dce39
Rishabh <mailrishabh1403@gmail.com>
2017-08-25 17:51:37 +0530

    add artistlist component

---
+++ src/components/artistList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ArtistList = props => {
+        return (
+            <div>
+                
+            </div>
+        );
+};
+
+export default ArtistList;
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import Banner from './banner.js';
+import ArtistList from './artistList.js';
+const URL_ARTISTS = 'http://localhost:3004/artists';
 
 export default class Home extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            artists : ''
+        }
     }
 
     render(){
